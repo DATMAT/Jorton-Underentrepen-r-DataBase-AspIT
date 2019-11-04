8269041e457beb571f7f3c52bba56728b9d985d1
Donzulo <mads5376@edu.campusvejle.dk>
2019-11-04 13:50:19 +0100

    Fixed MacAddress.cs

--- JudRepository/MacAddress.cs
+++ JudRepository/MacAddress.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace JudBizz
+namespace JudRepository
 {
     public class MacAddress
     {
